3ed5a3ddd68eff87bcceda81b46cd116a2594dae
Adam <guernica0131@gmail.com>
2015-03-23 22:01:18 +0500

    Changed flag

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (request, response) {
 
 var port = process.env.PORT || 8080;
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(port);
+server.listen(port); //
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:" + port + "/");
